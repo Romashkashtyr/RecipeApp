18c0f63c27b4587ef5d77f10bc56eca7877ed592
Romashkashtyr <Romainshtyr@yandex.ru>
2025-11-20 09:57:29 +0300

    Initial commit

---
+++ app/src/main/java/com/example/recipeapp/presentation/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.recipeapp.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.recipeapp.ui.theme.RecipeAppTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RecipeAppTheme {
+
+            }
+        }
+    }
+}
+
