9f02a8e151457d1fd2e7d08b9ceb5ce4945c2027
Romashkashtyr <Romainshtyr@yandex.ru>
2025-12-08 12:25:06 +0300

    making di, domain and data layers

--- app/src/main/java/com/example/recipeapp/domain/usecase/AddCategoryUseCase.kt
+++ app/src/main/java/com/example/recipeapp/domain/usecase/AddCategoryUseCase.kt
@@ -1,13 +1,14 @@
 package com.example.recipeapp.domain.usecase
 
+
 import com.example.recipeapp.domain.repository.MainRecipeRepository
 import javax.inject.Inject
 
 class AddCategoryUseCase @Inject constructor(
     private val repository: MainRecipeRepository
 ) {
-    suspend operator fun invoke(category: String) {
-        repository.addSubscription(category)
-        repository.updateRecipeForTopic(category)
+    suspend operator fun invoke(category: String, cuisine: String) {
+        repository.addSubscription(category, cuisine)
+        repository.updateRecipeForTopic(category, cuisine)
     }
 }
