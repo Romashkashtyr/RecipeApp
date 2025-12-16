e12273225203b0f670dfd68c2cd7ab4664e79a60
Romashkashtyr <Romainshtyr@yandex.ru>
2025-12-16 17:30:00 +0300

    making di, domain and data layers

--- app/src/main/java/com/example/recipeapp/presentation/screen/subscriptions/SubscriptionsViewModel.kt
+++ app/src/main/java/com/example/recipeapp/presentation/screen/subscriptions/SubscriptionsViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.recipeapp.presentation.screen.subscriptions
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.recipeapp.domain.entity.Recipe
 import com.example.recipeapp.domain.usecase.AddSubscriptionUseCase
 import com.example.recipeapp.domain.usecase.ClearAllCategoriesUseCase
@@ -11,6 +12,8 @@ import com.example.recipeapp.domain.usecase.UpdateSubscribedRecipesForAllCategor
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -30,22 +33,43 @@ class SubscriptionsViewModel @Inject constructor(
     fun processCommand(command: SubscriptionsCommand) {
         when(command) {
             SubscriptionsCommand.ClearRecipes -> {
-
+                viewModelScope.launch {
+                    val categories = state.value.selectedCategories
+                    clearAllCategoriesUseCase(categories)
+                }
             }
             SubscriptionsCommand.ClickSubscribe -> {
-
+                viewModelScope.launch {
+                    _state.update { previousState ->
+                        val categoryQuery = state.value.query.trim()
+                        val  categoryCuisine = state.value.cuisine.trim()
+                        addSubscriptionUseCase(categoryQuery, categoryCuisine)
+                        previousState.copy(query = categoryQuery, cuisine = categoryCuisine)
+                    }
+                }
             }
             is SubscriptionsCommand.InputCategory -> {
-
+                _state.update { previousState ->
+                    previousState.copy(query = command.query)
+                }
             }
             SubscriptionsCommand.RefreshData -> {
-
+                viewModelScope.launch {
+                    updateSubscribedRecipesForAllCategoriesUseCase()
+                }
             }
             is SubscriptionsCommand.RemoveSubscription -> {
-
+                viewModelScope.launch {
+                    removeSubscriptionUseCase(command.category, command.cuisine)
+                }
             }
             is SubscriptionsCommand.ToggleCategorySelection -> {
-
+                _state.update { previousState ->
+                    val subscriptions = previousState.subscriptions.toMutableMap()
+                    val isSelected = subscriptions[command.category] ?: false
+                    subscriptions[command.category]= !isSelected
+                    previousState.copy(subscriptions = subscriptions)
+                }
             }
         }
     }
@@ -63,12 +87,13 @@ sealed interface SubscriptionsCommand {
 
     data object ClearRecipes: SubscriptionsCommand
 
-    data class RemoveSubscription(val category: String): SubscriptionsCommand
+    data class RemoveSubscription(val category: String, val cuisine: String): SubscriptionsCommand
 }
 
 
 data class SubscriptionsState(
     val query: String = "",
+    val cuisine: String = "",
     val subscriptions: Map<String, Boolean> = mapOf(),
     val recipes: List<Recipe> = listOf()
 ) {
